e019b21b2debfdd351fb67a91ae1e9732e096c59
mckaylush <130932201+mckaylush@users.noreply.github.com>
2024-04-11 21:26:42 -0600

    In process ONNX

---
+++ Models/OrderPrediction.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildsByBrickwellNew.Models
+{
+    public class OrderPrediction
+    {   
+        public Order Orders { get; set; }
+        public string Prediction { get; set; }
+    }
+}
+
